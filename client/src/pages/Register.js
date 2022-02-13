51e15de5eb190dbc512c1bba7d12a52f1d96157a
Juan Carlos Valdes <idevkingos@gmail.com>
2022-02-13 16:20:19 -0300

    feat: Button component

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,14 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import Input from "../components/Input";
+import Button from "../components/Button";
 
 const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleClick = async () => {
+    console.log(name, email, password);
+  };
+
   return (
     <div className="d-flex justify-content-center" style={{ height: "80vh" }}>
       <div className="container align-items-center d-flex">
         <div className="row col-md-6 offset-md-3 text-center">
-          <h1 className="pt-5 fw-bold">Lets get started</h1>
+          <h1 className="pt-5 fw-bold">Let's Get Started</h1>
           <p className="lead pb-4">
             Sign up for free. No credit card required.
           </p>
+
+          <div className="form-group">
+            <Input label="Name" value={name} setValue={setName} />
+            <Input
+              label="Email"
+              type="email"
+              value={email}
+              setValue={setEmail}
+            />
+            <Input
+              label="Password"
+              type="password"
+              value={password}
+              setValue={setPassword}
+            />
+
+            <div className="d-grid">
+              <Button
+                handleClick={handleClick}
+                type="danger"
+                text="Register"
+                size="sm"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
